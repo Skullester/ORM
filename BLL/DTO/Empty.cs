51f836046488545ab5e5467739f1dd6e0ca979d4
Alexander <138435791+Skullester@users.noreply.github.com>
2024-12-04 01:45:29 +0400

    done it

---
+++ BLL/DTO/Empty.cs
@@ -0,0 +1,12 @@
+﻿namespace BLL.DTO;
+
+public class Empty : IElementDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public Empty(string name)
+    {
+        Name = name;
+    }
+}
